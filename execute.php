a9bfe6f8a4d5cf5fadc505536adba0c0c3f5222c
Naituolf <62960961+Naituolf@users.noreply.github.com>
2020-03-31 22:29:39 +0200

    Update execute.php

--- execute.php
+++ execute.php
@@ -14,7 +14,7 @@ $firstname = isset($message['chat']['first_name']) ? $message['chat']['first_nam
 $lastname = isset($message['chat']['last_name']) ? $message['chat']['last_name'] : "";
 $username = isset($message['chat']['username']) ? $message['chat']['username'] : "";
 $date = isset($message['date']) ? $message['date'] : "";
-$randomNumber = rand(1,99);
+$randomNumber = 1;
 $uno = "http://randomwaifu.altervista.org/images/00";
 $tre = "randomNumber.png";
 // la variabile $stringa conterrà la
