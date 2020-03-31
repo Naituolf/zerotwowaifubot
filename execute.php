d1b89ec43bfa8149ff5eb60140906f7c808568e3
Naituolf <62960961+Naituolf@users.noreply.github.com>
2020-03-31 23:33:04 +0200

    Update execute.php

--- execute.php
+++ execute.php
@@ -1,7 +1,6 @@
 <?php
 $content = file_get_contents("php://input");
 $update = json_decode($content, true);
-$counter+=1;
 
 $message = isset($update['message']) ? $update['message'] : "";
 $messageId = isset($message['message_id']) ? $message['message_id'] : "";
@@ -11,17 +10,45 @@ $lastname = isset($message['chat']['last_name']) ? $message['chat']['last_name']
 $username = isset($message['chat']['username']) ? $message['chat']['username'] : "";
 $date = isset($message['date']) ? $message['date'] : "";
 
-
-if($counter>=10)
+$fp = fopen(counter.txt, r);
+$counter=fread($fp, 1);
+if($counter==1)
 {
-  $counter=0;
-  $numero=rand(1, 99);
-  $text = "http://randomwaifu.altervista.org/images/00".$numero.".png";
-  header("Content-Type: application/json");
-  $parameters = array('chat_id' => $chatId, "text" => $text);
-  $parameters["method"] = "sendMessage";
-  echo json_encode($parameters);
+  $counter=fread($fp, 1);
+  if($counter==0)
+  {
+    fclose($fp);
+    $fp = fopen(counter.txt, w+);
+
+    fwrite($fp, $counter);
+    fclose($fp);
+    $numero=rand(1, 99);
+    $text = "http://randomwaifu.altervista.org/images/00".$numero.".png";
+    header("Content-Type: application/json");
+    $parameters = array('chat_id' => $chatId, "text" => $text);
+    $parameters["method"] = "sendMessage";
+    echo json_encode($parameters);
+  }
+  else
+    fclose($fp);
+    $counter+=1;
+    $fp = fopen(counter.txt, w+);
+
+    fwrite($fp, $counter);
+    fclose($fp);
 }
+else
+  fclose($fp);
+  $counter+=1;
+  $fp = fopen(counter.txt, w+);
+
+  fwrite($fp, $counter);
+  close($fp);
+
+
+// Ora apro il file in lettura, mi muovo al suo interno, e stampo parti di contenuto
+
+
 
 
 if((strpos($message['text'], 'ferr') !== false)or(strpos($message['text'], 'Ferr') !== false))
