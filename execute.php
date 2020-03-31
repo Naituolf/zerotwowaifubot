facfa41689183269e40497fbf1340ce9d5c238c8
Naituolf <62960961+Naituolf@users.noreply.github.com>
2020-03-31 23:35:11 +0200

    Update execute.php

--- execute.php
+++ execute.php
@@ -10,42 +10,6 @@ $lastname = isset($message['chat']['last_name']) ? $message['chat']['last_name']
 $username = isset($message['chat']['username']) ? $message['chat']['username'] : "";
 $date = isset($message['date']) ? $message['date'] : "";
 
-$fp = fopen(counter.txt, r);
-$counter=fread($fp, 1);
-if($counter==1)
-{
-  $counter=fread($fp, 1);
-  if($counter==0)
-  {
-    fclose($fp);
-    $fp = fopen(counter.txt, w+);
-
-    fwrite($fp, $counter);
-    fclose($fp);
-    $numero=rand(1, 99);
-    $text = "http://randomwaifu.altervista.org/images/00".$numero.".png";
-    header("Content-Type: application/json");
-    $parameters = array('chat_id' => $chatId, "text" => $text);
-    $parameters["method"] = "sendMessage";
-    echo json_encode($parameters);
-  }
-  else
-    fclose($fp);
-    $counter+=1;
-    $fp = fopen(counter.txt, w+);
-
-    fwrite($fp, $counter);
-    fclose($fp);
-}
-else
-  fclose($fp);
-  $counter+=1;
-  $fp = fopen(counter.txt, w+);
-
-  fwrite($fp, $counter);
-  close($fp);
-
-
 // Ora apro il file in lettura, mi muovo al suo interno, e stampo parti di contenuto
 
 
